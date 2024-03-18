27addf4c921e18bd6c881384715a07ae2c6fcb93
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2024-03-18 20:44:46 +0000

    done

--- 01_variable.js
+++ 01_variable.js
@@ -16,6 +16,6 @@ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 
 
 /*
-prefer not to use war because of issue
+prefer not to use var because of issue
  in block scope or functional scope  
 */
