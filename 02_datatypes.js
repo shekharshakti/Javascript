cfe54a1352cf10ad1b656fbfe3621166c5e5c010
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2024-03-18 20:44:27 +0000

    datadone

---
+++ 02_datatypes.js
@@ -0,0 +1,16 @@
+let name="shekhar"
+let age=20
+let isLoggedIn=false
+let state;
+
+//number
+//bigint
+//string=""
+//boolean=->true,false
+//null ==> standalone value
+//undefined==>
+//symbo
+
+//object
+
+console.log(typeof name);
