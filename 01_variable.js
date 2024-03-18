755f96edc27181c7f8fbddced452c73b694df310
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2024-03-18 19:01:25 +0000

    variabledone

---
+++ 01_variable.js
@@ -0,0 +1,21 @@
+//variable are container to store data of different datatypes
+const accountId=12201493
+let accountPassword="shekhar"
+var accountEmail="shekharshakti7050@gmail.com"
+accountCity="Punjab"
+let accountState;  //output will be undefined
+
+console.log(accountId)
+//accountId=1220493//not allowed in case of const
+accountEmail="shekharshakti"
+accountPassword="shakti"
+//const can be changed
+
+//console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+/*
+prefer not to use war because of issue
+ in block scope or functional scope  
+*/
