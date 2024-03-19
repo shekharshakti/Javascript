f93c34b3aca4e2a37ff4da910685578c8428159f
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2024-03-19 05:07:27 +0000

    done

---
+++ 04_arithemeticOperation.js
@@ -0,0 +1,98 @@
+// Number + Number => Addition  
+let x = 1 + 2 
+console.log(x) 
+  
+// Number + String => Concatenation 
+let y =  5 + "hello" 
+console.log(y) 
+
+// Number - Number => Subtraction  
+let a = 10 - 7  
+console.log(a) 
+  
+let b = "Hello" - 1  
+console.log(b) 
+
+// Number * Number => Multiplication 
+let c = 3 * 3  
+let d= -4 * 4  
+console.log(c) 
+console.log(d) 
+  
+let e= Infinity * 0  
+let f = Infinity * Infinity  
+console.log(e) 
+console.log(f) 
+let z = 'hi' * 2  
+console.log(z) 
+
+// Number / Number => Division 
+let g = 5 / 2 
+let h = 1.0 / 2.0 
+console.log(g) 
+console.log(h) 
+  
+  
+let i = 3.0 / 0 
+let j = 4.0 / 0.0 
+console.log(i) 
+console.log(j) 
+let k = 2.0 / -0.0 
+console.log(k)
+
+
+// Number % Number => Modulus of the number 
+let l = 9 % 5 
+let m = -12 % 5 
+let n = 1 % -2 
+let o = 5.5 % 2 
+let p = -4 % 2 
+let q = NaN % 2 
+  
+console.log(l) 
+console.log(m) 
+console.log(n) 
+console.log(o) 
+console.log(p) 
+console.log(q) 
+
+// Postfix  
+let r = 2; 
+s = r++; // b = 2, a = 3 
+  
+// Prefix 
+let t = 5; 
+u = ++x; // x = 6, y = 6 
+  
+console.log(r) 
+console.log(s) 
+console.log(t) 
+console.log(u) 
+
+// Prefix 
+let v = 2; 
+v1 = --v;  
+  
+// Postfix  
+let a1 = 3; 
+b1= a1--; 
+   
+console.log(v) 
+console.log(v1) 
+console.log(a1) 
+console.log(b1)
+
+
+let c1 = 3; 
+d1 = -c1;  
+  
+// Unary negation operator 
+// can convert non-numbers 
+// into a number 
+let d2 = "3"; 
+e1 = -d2;  
+    
+console.log(c1) 
+console.log(d1) 
+console.log(d2) 
+console.log(e1) 
