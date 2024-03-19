ce10241b609f57be24ccea2b25ed29a23bba24af
Shekhar Shakti <119730877+shekharshakti@users.noreply.github.com>
2024-03-19 18:12:09 +0000

    done

---
+++ 05_comparisonOperator.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 10;
+
+console.log("Equal to (==):", a == b);        // false
+console.log("Not equal to (!=):", a != b);    // true
+console.log("Greater than (>):", a > b);      // false
+console.log("Less than (<):", a < b);         // true
+console.log("Greater than or equal to (>=):", a >= b);  // false
+console.log("Less than or equal to (<=):", a <= b);     // true
+console.log("Identity (===):", a === b);      // false
+console.log("Non-identity (!==):", a !== b); // true
